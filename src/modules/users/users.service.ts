9f1063f53edec64a4b840ac0f293cce881546f26
Elvmx <elvmx47@aliyun.com>
2020-04-02 23:03:59 +0800

    usersService 创建用户

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  /**
+   * 创建用户
+   * @param data 用户信息
+   */
+  async create(data: CreateUserDto) {
+    const { username } = data;
+    const user = await this.findOne(username);
+
+    if (user) {
+      throw new BadRequestException('用户名已存在');
+    }
+
+    const entity = this.usersRepository.create(data);
+    return this.usersRepository.save(entity);
+  }
+
+  /**
+   * 根据 username 查找用户
+   * @param username 用户名
+   */
+  async findOne(username: string) {
+    return await this.usersRepository.findOne({ username });
+  }
+}
